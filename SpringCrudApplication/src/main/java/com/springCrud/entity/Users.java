bbd40a11f6058ef331baa7e2287ed5a3bf54e63d
BILL GRAHAM <grahambill011@gmail.com>
2021-09-14 21:49:09 +0300

    Update Users.java

--- SpringCrudApplication/src/main/java/com/springCrud/entity/Users.java
+++ SpringCrudApplication/src/main/java/com/springCrud/entity/Users.java
@@ -37,6 +37,38 @@ public class Users {
 		this.age = age;
 		this.salary = salary;
 	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
 	
 	
 }
