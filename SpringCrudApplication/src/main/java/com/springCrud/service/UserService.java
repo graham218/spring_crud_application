49ccd98304ec7ce8ac5cd83f86e0268ed2dea45b
BILL GRAHAM <grahambill011@gmail.com>
2021-09-15 00:29:17 +0300

    Update UserService.java

--- SpringCrudApplication/src/main/java/com/springCrud/service/UserService.java
+++ SpringCrudApplication/src/main/java/com/springCrud/service/UserService.java
@@ -1,5 +1,12 @@
 package com.springCrud.service;
 
+import java.util.List;
+
+import com.springCrud.entity.Users;
+
 public interface UserService {
 
+	public List<Users> getAllUsers();
+	public Users getUserById(int userId);
+	
 }
